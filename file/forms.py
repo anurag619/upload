b521bcb89a4ac0258e0d324ec06b8f6f966c6611
anurag kumar <anurag3rdsep@gmail.com>
2013-10-23 00:22:04 +0530

    almost done

--- file/forms.py
+++ file/forms.py
@@ -3,7 +3,6 @@ from file.models import file_file
 
 class file_fileForm(forms.ModelForm):
     name = forms.CharField(max_length=50, help_text="enter the title of the file to add")
-    
     files = forms.FileField()
     
     class Meta:
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+upload
+=======
+
+simple django code to upload a file from the user. 
