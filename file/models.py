79e1c7978661291797db8347fe318c4267fc5c74
anurag kumar <anurag3rdsep@gmail.com>
2013-10-22 23:52:53 +0530

    rm date/time

--- file/models.py
+++ file/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class file_file(models.Model):
     name = models.CharField(max_length=255)
     files = models.FileField(upload_to='file/%Y/%m/%d')
-    pub_date = models.DateTimeField('date published')
+    
 
     def __unicode__(self):  
         return self.name
